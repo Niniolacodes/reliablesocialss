fae58128817164365dd37e9db64d2eda7a1e01bf
Niniola Code <niniolaifeoluwa54@gmail.com>
2026-05-05 20:12:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,37 @@ menuBtn.addEventListener("click", () => {
 
 
 
+const heroSlides = [
+  {
+    icon: "📶",
+    title: 'Buy <span class="text-blue-600">Data</span>',
+    text: "Stay Connected with Cheap Data.",
+  },
+  {
+    icon: "🛡️",
+    title: 'SMS <span class="text-blue-600">Verification</span>',
+    text: "Temp numbers for seamless OTP.",
+  },
+  {
+    icon: "🚀",
+    title: 'Social <span class="text-blue-600">Boost</span>',
+    text: "Buy engagements for TikToks, Instagrams, etc.",
+  },
+];
+
+let heroIndex = 0;
+
+const heroIcon = document.getElementById("heroIcon");
+const heroTitle = document.getElementById("heroTitle");
+const heroText = document.getElementById("heroText");
+
+setInterval(() => {
+  heroIndex = (heroIndex + 1) % heroSlides.length;
+
+  heroIcon.textContent = heroSlides[heroIndex].icon;
+  heroTitle.innerHTML = heroSlides[heroIndex].title;
+  heroText.textContent = heroSlides[heroIndex].text;
+}, 1500);
 
 
 
