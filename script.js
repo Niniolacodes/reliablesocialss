b11178d08120676405a7f9be292f888de22923d0
Niniola Code <niniolaifeoluwa54@gmail.com>
2026-05-05 19:10:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,12 @@
 const menuBtn = document.getElementById("menuBtn");
 const mobileMenu = document.getElementById("mobileMenu");
 
-menuBtn.addEventListener("click", function () {
+menuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 
   if (mobileMenu.classList.contains("hidden")) {
     menuBtn.textContent = "☰";
   } else {
-    menuBtn.textContent = "✕";
+    menuBtn.textContent = "×";
   }
-});
-
-const heroItems = [
-  {
-    icon: "📶",
-    title: 'Buy <span class="text-blue-600">Data</span>',
-    text: "Stay Connected with Cheap Data.",
-  },
-  {
-    icon: "🛡️",
-    title: 'SMS <span class="text-blue-600">Verification</span>',
-    text: "Temp Numbers for seamless OTP.",
-  },
-  {
-    icon: "🚀",
-    title: 'Social <span class="text-blue-600">Boost</span>',
-    text: "Buy engagement for TikToks, Instagrams etc.",
-  },
-];
-
-let currentItem = 0;
-
-const switchIcon = document.getElementById("switchIcon");
-const switchTitle = document.getElementById("switchTitle");
-const switchText = document.getElementById("switchText");
-
-setInterval(function () {
-  currentItem = (currentItem + 1) % heroItems.length;
-
-  switchIcon.textContent = heroItems[currentItem].icon;
-  switchTitle.innerHTML = heroItems[currentItem].title;
-  switchText.textContent = heroItems[currentItem].text;
-}, 3000);
-
-const registerBtn = document.querySelector(".primary-btn");
-const loginBtn = document.querySelector(".login-btn");
-const downloadBtn = document.querySelector(".secondary-btn");
-
-registerBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  alert("Register page coming soon");
-});
-
-loginBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  alert("Login page coming soon");
-});
-
-downloadBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  alert("App download coming soon");
 });
